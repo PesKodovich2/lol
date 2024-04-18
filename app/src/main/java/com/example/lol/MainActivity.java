37d98b613f6012b77ff55385228506484ae07536
PesKodovich <alenakostyakova007@gmail.com>
2024-04-18 13:56:36 +0300

    delete comment

--- app/src/main/java/com/example/lol/MainActivity.java
+++ app/src/main/java/com/example/lol/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-// comment
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
